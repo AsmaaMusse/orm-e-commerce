ee845642b94dfb90bd0c8287a920e125c2271428
Asmaa <Asmaa99musse@gmail.com>
2021-12-16 06:52:54 +0000

    added foreign keys

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,3 @@
-// import models
 const Product = require("./Product");
 const Category = require("./Category");
 const Tag = require("./Tag");
@@ -6,23 +5,34 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
 
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-  foreignKey: {
-    name: ProductTag,
-  },
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+
+  through: { ProductTag },
 });
 
 // Tags belongToMany Products (through ProductTag)
 
-Tag.belongsToMany(Product);
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+
+  through: { ProductTag },
+});
 
 module.exports = {
   Product,
