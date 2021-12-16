8b22e20fde545a31adaa2cb316c8f5fa94637977
Asmaa <Asmaa99musse@gmail.com>
2021-12-16 07:22:06 +0000

    created try catch functions

--- src/routes/api/category-routes.js
+++ src/routes/api/category-routes.js
@@ -1,31 +1,61 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
-const { Category, Product } = require('../../models');
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:getCategoryRouter" });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:getCategoryRouterById" });
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:postCategoryRouter" });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:putCategoryRouter" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  try {
+    res.status(200).json();
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Error!:deleteCategoryRouter" });
+  }
 });
 
 module.exports = router;
