d25c146124ee14d7a52c62d577c1fd0b0616ea85
Asmaa <Asmaa99musse@gmail.com>
2021-12-18 03:15:08 +0000

    turned on connection to db and server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 
-const connection = require('./config/connection');
+const connection = require("./config/connection");
+const { sequelize } = require("./models/Product");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 4000;
 
@@ -14,16 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-const init = async () => {
-  try {
-    await connection.sync({ force: false });
-
-    app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`)
-    );
-  } catch (error) {
-    console.log(`[ERROR]: Connection to DB failed - ${error.message}`);
-  }
-};
-
-init();
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+  );
+});
